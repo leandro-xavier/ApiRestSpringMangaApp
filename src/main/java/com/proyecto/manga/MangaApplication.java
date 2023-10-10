47126171b5e8c3dd54528686f30f68e3728c9c5f
leandro <leaxavier2014@gmail.com>
2023-10-09 20:22:07 -0500

    inizializando repositorio

---
+++ src/main/java/com/proyecto/manga/MangaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.manga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaApplication.class, args);
+	}
+
+}
